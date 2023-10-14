2c0cfa1c09506e61758b9ec685257e5033ec01ba
Emile <bvcxzq1@gmail.com>
2023-10-14 12:27:10 +0000

    Add serializer for likes app

---
+++ likes/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Like
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    The create method handles the unique constraint on 'owner' and 'post'
+    """
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'id', 'owner', 'post', 'created_at',
+        ]
