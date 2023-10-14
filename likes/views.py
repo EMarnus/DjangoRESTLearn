493a4f1688e09fe1d6919ebcaaab2a164a246ca3
Emile <bvcxzq1@gmail.com>
2023-10-14 14:32:45 +0000

    Refactor Post and Profile views to use generic

--- likes/views.py
+++ likes/views.py
@@ -5,9 +5,6 @@ from .serializers import LikeSerializer
 
 
 class likeList(generics.ListCreateAPIView):
-    """
-    List comments or create a comment if logged in.
-    """
     serializer_class = LikeSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Like.objects.all()
@@ -17,9 +14,6 @@ class likeList(generics.ListCreateAPIView):
 
 
 class likeDetail(generics.RetrieveDestroyAPIView):
-    """
-    Retrieve a comment, or update or delete it by id if you own it.
-    """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
