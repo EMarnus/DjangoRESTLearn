9c5dad8f18cb01547d927cf34cb67b12d9166f0f
Emile <bvcxzq1@gmail.com>
2023-10-14 12:47:44 +0000

    Add likes views and urls

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class likeList(generics.ListCreateAPIView):
+    """
+    List comments or create a comment if logged in.
+    """
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class likeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a comment, or update or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
