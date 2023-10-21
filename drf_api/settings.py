f0b3ee95047cb44531dd533f0fd3c9220860b406
Emile <bvcxzq1@gmail.com>
2023-10-21 09:34:17 +0000

    Remove debug true

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-emarnus-djangorestlearn-d9m06jdae1q.ws-eu105.gitpod.io',
